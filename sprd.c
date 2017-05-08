b0792f35969714e19876259e7e4b1964639277b8
Ray <rayluo00@gmail.com>
2017-05-08 15:19:34 -0700

    fix typo on report and edit spacing

--- sprd.c
+++ sprd.c
@@ -34,7 +34,7 @@
 #define CONFIG_FILE "sprd.conf"
 
 typedef struct reservation {
-    int inUse; // 1 if port is currently being securely bound to by another process; 0 otherwise
+    int inUse; // 1 if port i securely bound to another process; 0 otherwise
     int fd;    // fd for the socket created when the daemon bound to this port
     range_node* uid_head;
     range_node* gid_head;
@@ -94,7 +94,8 @@ int main () {
 
   // Change the current working directory to root
   if (chdir("/") < 0) {
-    syslog(LOG_CRIT, "Unable to change daemon process's current working directory to '/'");
+    syslog(LOG_CRIT, 
+            "Unable to change daemon process's current working directory to '/'");
     exit(EXIT_FAILURE);
   }
 
@@ -108,30 +109,37 @@ int main () {
   // Close the standard file descriptors
   close(STDIN_FILENO);
   close(STDOUT_FILENO);
-  //close(STDERR_FILENO);
+  close(STDERR_FILENO);
 
   // Parse config file to generate a linked list of reservations
   // IMPORTANT: change this path to point to where the config file should really be
   r = parse_config("/home/rickse2/Documents/csci49X/secure-port-reservation/sprd.conf");
+
   // Step through list of reservations, binding to each port and storing the socket fd
   // TODO: see if there's a nice way to reduce amount of duplicate code in this loop
   memset(&resList, 0, sizeof(resList));
   struct sockaddr_in sockInfo;
+
   while (r != NULL) {
     range_node* ports = r->port_head;
+
     while (ports != NULL) {
         // Bind to first port in range
         int portNum = ports->range[0];
+
         // TODO: make system call to get lower port number
         if (portNum > LOWER_PORT_LIMIT && portNum < NUM_PORTS) {
             memset(&sockInfo, 0, sizeof(sockInfo));
             sockInfo.sin_family = AF_INET;
             sockInfo.sin_port = htons(portNum);
+
             if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
                 // call to socket() failed
-                syslog(LOG_ALERT, "Unable to acquire a file descriptor for port %d", portNum);
+                syslog(LOG_ALERT, 
+                        "Unable to acquire a file descriptor for port %d", portNum);
                 syslog(LOG_ALERT, "Unable to reserve port %d", portNum);
-            } else if (bind(fd, (struct sockaddr *)&sockInfo, sizeof(sockInfo)) < 0) {
+            } 
+            else if (bind(fd, (struct sockaddr *)&sockInfo, sizeof(sockInfo)) < 0) {
                 // call to bind() failed
                 syslog(LOG_ALERT, "Unable to reserve port %d", portNum);
             } else {
@@ -144,17 +152,23 @@ int main () {
             // Not a valid port number
             syslog(LOG_WARNING, "Invalid port number: %d", portNum);
         }
+
         for (i = portNum+1; i <= ports->range[1]; i++) {
             portNum = i;
+
             if (portNum > LOWER_PORT_LIMIT && portNum < NUM_PORTS) {
                 memset(&sockInfo, 0, sizeof(sockInfo));
                 sockInfo.sin_family = AF_INET;
                 sockInfo.sin_port = htons(portNum);
+
                 if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
                     // call to socket() failed
-                    syslog(LOG_ALERT, "Unable to acquire a file descriptor for port %d", portNum);
+                    syslog(LOG_ALERT, 
+                            "Unable to acquire a file descriptor for port %d", portNum);
                     syslog(LOG_ALERT, "Unable to reserve port %d", portNum);
-                } else if (bind(fd, (struct sockaddr *)&sockInfo, sizeof(sockInfo)) < 0) {
+                } 
+                else if (bind(fd, (struct sockaddr *)&sockInfo, 
+                            sizeof(sockInfo)) < 0) {
                     // call to bind() failed
                     syslog(LOG_ALERT, "Unable to reserve port %d", portNum);
                 } else {
@@ -168,8 +182,10 @@ int main () {
                 syslog(LOG_WARNING, "Invalid port number: %d", portNum);
             }
         }
+
         ports = ports->next;
     }
+
     r = r->next;
   }
 
@@ -192,26 +208,30 @@ int main () {
   while(1) {
     // TODO: Get uds socket then FD_SET the uds socket into the active_fset
     read_fdset = active_fdset;
+
     if (select (FD_SETSIZE, &read_fdset, NULL, NULL, NULL) == 0) {
       for (i = 0; i < FD_SETSIZE; i++) {
 	    if (FD_ISSET(i, &read_fdset)) {
           if (i == namedFifo) {
-	        /* run secure_bind interface, read uds name from named fifo, try to connect to that uds */
+	        /* Run secure_bind interface, read uds name from named fifo, try 
+             * to connect to that uds */
 	        handleNewConnection(namedFifo, &active_fdset);
-	        /* This is the case where it is not the named fifo, so it is one of possibly many ud sockets */
-	        /* Need to check what was sent across, if 0, shutdown connection and FD_CLR, if not 0, then they have sent credentials/port request */
-          } else {
-	        //fprintf(stderr, "SELECT: %d", uds); //test Ray -- can we delete this?
+	      }
+
+          /* This is the case where it is not the named fifo, so it is one 
+           * of possibly many ud sockets. Need to check what was sent across, 
+           * if 0, shutdown connection and FD_CLR, if not 0, then they have 
+           * sent credentials/port request */
+          else {
 	        memset(portBuf, 0, sizeof(portBuf));
+
 	        if ((recv(i, portBuf, PORT_DIGIT_MAX, MSG_PEEK)) < 0) {
               syslog(LOG_ERR, "Failed to recieve data from file descriptor %d", i);
 	          break;
-	        } else if (strlen(portBuf) == 0) {
-	          /* This is the case of secure_close, i can be closed and FD_CLR'ed from active_fdset */
-
-		  // Ray -- note to check through udsTable (udsTableIndex is the max index currently) for i == uds, grab port num
-		  // delete that entry in the udsTable and subtract udsTableIndex by 1, use port num to access resList, mark port as not in use
-		  
+	        } 
+            else if (strlen(portBuf) == 0) {
+	          /* This is the case of secure_close, i can be closed 
+               * and FD_CLR'ed from active_fdset. */
                 if (close(i) < 0) {
                     syslog(LOG_CRIT, "Failed to close the file descriptor %d", i);
                 }
@@ -219,14 +239,14 @@ int main () {
                 FD_CLR(i, &active_fdset);
 	        } else {
 	          /* This is the case of secure_bind */
-	          if((handleExistingConnection(i, portBuf, &udsTableIndex, udsTable, resList)) < 0) {
-		    FD_CLR(i, &active_fdset);
-		    if((close(i)) < 0) {
-		      syslog(LOG_CRIT, "Failed to shut down the file descriptor %d", i);
-		    }
-		  }
+	          if((handleExistingConnection(i, portBuf, &udsTableIndex, 
+                                           udsTable, resList)) < 0) {
+		        FD_CLR(i, &active_fdset);
+		        if((close(i)) < 0) {
+		          syslog(LOG_CRIT, "Failed to shut down the file descriptor %d", i);
+		        }
+		      }
 	        }
-
           }
 	    }
 	  }
@@ -236,8 +256,8 @@ int main () {
   }
 }
 
-/* Function to handle a request on a unix domain socket, return 0 on success, -1 on failure */
-
+/* Function to handle a request on a unix domain socket, return 0 on success, 
+ * -1 on failure */
 int handleExistingConnection (int uds, char *portBuf, int *udsTableIndex, udsToPortList *udsTable, reservation *resList) {
 
   struct msghdr credMsg, fdMsg;
@@ -303,7 +323,8 @@ int handleExistingConnection (int uds, char *portBuf, int *udsTableIndex, udsToP
     
   }
 
-  if(!(is_in_range(resList[port].uid_head, (int)realCred->uid) || is_in_range(resList[port].gid_head, (int)realCred->gid))) {
+  if(!(is_in_range(resList[port].uid_head, (int)realCred->uid) || 
+              is_in_range(resList[port].gid_head, (int)realCred->gid))) {
 
     /* Credentials do not match */
 
